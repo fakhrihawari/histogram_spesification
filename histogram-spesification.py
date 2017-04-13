9de3322883914fd6394cfd9a3aa04139ff975d5f
fha <blitzfakhri@gmail.com>
2017-04-13 13:20:01 +0700

    Update and rename mini-hspesifikasi.py to histogram-spesification.py
    implementation of histogram specification

--- histogram-spesification.py
+++ histogram-spesification.py
@@ -15,8 +15,6 @@ def hspesifikasi2(Hx,Hz):
         #print i,lookup2[i]
 
     return lookup
-
 Hz=[0,0,0,0.15,0.35,0.65,0.85,1]
 Hx=[0.19,0.44,0.65,0.81,0.89,0.95,0.98,1]
 result=hspesifikasi2(Hx,Hz)
-#print coba
